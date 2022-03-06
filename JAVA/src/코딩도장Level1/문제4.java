1a22d056dc5ed9d267845d60f684c08b3bb2d88f
82104 <82104@DESKTOP-MA6SH7U>
2022-03-06 15:58:52 +0900

    day04

--- JAVA/src/코딩도장Level1/문제4.java
+++ JAVA/src/코딩도장Level1/문제4.java
@@ -1,5 +1,24 @@
 package 코딩도장Level1;
 
-public class 문제4 {
+import java.util.Scanner;
 
-}
+public class 문제4 { // c  s
+
+	public static void main(String[] args) { // m  s
+		
+//		숫자를 입력받으면 그에해당하는 자릿수를 출력하는 코드를 작성하라.
+//		입력 : 156 	->	출력 : 100의자리수
+//		입력 : 18961 ->	출력 : 10000의자릿수
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요"); int number = scanner.nextInt(); // 숫자를 입력받음
+		
+		// 	숫자배열에서 몇글자인지를 판단하려면?	for(int i = 1; i <= number.(); i++) //
+		
+			System.out.println("입력받은 숫자에 해당하는 자릿수 " );
+		
+		
+	} // m  e
+} // c  e
+
