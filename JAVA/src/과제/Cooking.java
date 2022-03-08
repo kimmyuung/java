d1ae97d550bb7a71d6b7bea9ec55e5765a2110c9
504 <504@DESKTOP-F07912N>
2022-03-08 20:52:25 +0900

    day06

--- JAVA/src/과제/Cooking.java
+++ JAVA/src/과제/Cooking.java
@@ -1,5 +1,68 @@
 package 과제;
 
-public class Cooking {
+import java.util.Scanner;
 
-}
+import Day06.Board;
+
+public class Cooking { // cs
+	public static void main(String[] args) { // ms
+	
+		// 라면레시피
+		// 초기메뉴   [1. 라면 선택      2. 라면 끓이기]
+		// 0. 설계 
+		// 라면레시피 클래스를 만들어 넣음(재료 : 라면 1봉, 물, 계란, 파 -> 클래스의 필드
+		// 생성자 사용 O
+		// 1-1. 사용자로부터 먹는 라면 레시피를 입력받음
+		// 1-2. 레시피 등록(라면, 물, 계란, 파)
+		// 2. 라면을 끓이기 위한 조리과정 진행
+		// 분기 1 : 라면을 끓일 때 스프를 먼저 넣어야 되나 면을 먼저 넣어야 되나
+		// 변수 선언
+		Scanner scanner = new Scanner(System.in); // 입력 객체 선언
+		Ramen[] recipelist = new Ramen[100];
+		
+		while(true) { // w s
+		System.out.println("-------라면 조리---------"); // 초기 메뉴 출력
+		System.out.println("나만의 라면 레세피를 등록 후 라면을 맛있게 끓일 레시피를 공유하세요!(1개 기준)");
+		System.out.println("라면 종류\t 조리방법 "); // 
+		int index = 0; // 인덱스 변수 [반복순서 체크]
+		
+		// 모든 게시물조회
+			for(Ramen temp : recipelist) { // for s
+				if(temp != null) { // if s
+				System.out.printf("%d  %s\t%s \n", index, temp.ramen, temp.cook);
+				} // if e ,
+				index++;
+			} // for e
+		System.out.println("1. 레시피 등록 2. 등록된 레시피 확인 ");
+		int select = scanner.nextInt();// 사용자로부터 실행시킬 메뉴를 입력받음
+		
+		if(select == 1) { // 레시피 등록
+			System.out.println("----- 라면 레시피 등록 페이지 -----");
+			System.out.println("라면종류 : ");	String ramen = scanner.next();
+			System.out.println("추가재료 : ");	String cook = scanner.next();
+			
+			Ramen recipe = new Ramen(ramen, cook); // 2개 필드를 가지는 생성자 사용
+			int i = 0; // 인덱스 변수[반복순서 체크]
+			for(Ramen temp : recipelist) { // for2 s
+			if(temp == null) { // if2 s -> 빈 공간을 찾으면
+				recipelist[i] = recipe; // 해당 인덱스에 새로운 객체 저장
+				break; // 한번 저장하면 반복문 종료
+				} // if2 e	
+				i++; // 인덱스를 1증가시킴
+			} // for2 e
+		}
+		
+		else if(select == 2) { // 등록된 레시피 확인
+			System.out.println("등록된 라면 레시피를 확인해주세요");
+			System.out.println("게시물 번호를 입력해주세요");
+			int bno = scanner.nextInt(); // 현 게시물 인덱스번호(=배열내 인덱스 값)
+			System.out.printf("추가재료 : %s   라면종류 : %s\n", recipelist[bno].cook, recipelist[bno].ramen);
+			
+			
+		}
+		else {System.out.println("알 수 없는 숫자입니다.[재입력]");}
+		
+		
+		}// we
+	}// me 
+} // c e
--- JAVA/src/과제/Updown.java
+++ JAVA/src/과제/Updown.java
@@ -1,5 +1,203 @@
 package 과제;
 
-public class Updown {
+import java.util.Scanner;
 
-}
+public class Updown { // c s
+	public static void main(String[] args) { // m s
+
+	// 엘리베이터
+	// 동작 : 사용자로부터 층을 입력받아서 엘레베이터 이동 
+	// 위로 이동 
+	// 아래로 이동  
+	Scanner scanner = new Scanner(System.in); // 입력객체 선언
+	while(true) { // 무한루프 종료조건 : X /// w s
+	System.out.println("------- 엘리베이터 --------"); // 엘레베이터 출력
+	System.out.println("1층   2층   3층   4층   5층");
+	System.out.println("------------------------");
+	System.out.println(">> 현재 사용자가 있는 층 선택 : ");
+	int select = scanner.nextInt(); // 현재 사용자가 있는 층을 입력받음
+	
+	if(select == 1) { // 1층 s
+		System.out.printf("현재 %d층에 있습니다.\n", select);
+		System.out.println("몇층으로 가기를 원하십니까");
+		int select2 = scanner.nextInt();
+		if(select2 == 1) { System.out.println("알림)) 1층은 현재 층입니다."); }
+		// 1층은 현재층
+		else if(select2 == 2) { // 1층에서 2층으로 가는 경우
+			System.out.println("2층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("2층에 도착했습니다.");
+		} // 1층에서 2층으로 가는 경우 e
+		
+		else if(select2 == 3) { // 1층에서 3층으로 가는 경우
+			System.out.println("3층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("3층에 도착했습니다.");
+		}
+		
+		else if(select2 == 4) { // 1층에서 4층으로 가는 경우
+			System.out.println("4층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("4층에 도착했습니다.");
+		}
+		
+		else if(select2 == 5) { // 1층에서 5층으로 가는 경우
+			System.out.println("5층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("5층에 도착했습니다.");
+		}
+	} // 1층 e
+	else if(select == 2) { // 2층 s
+		System.out.printf("현재 %d층에 있습니다.\n", select);
+		System.out.println("몇층으로 가기를 원하십니까");
+		int select2 = scanner.nextInt();
+		if(select2 == 1) { // 2층에서 1층으로 가는 경우
+			System.out.println("1층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("1층에 도착했습니다.");
+		} // 2층에서 1층으로 가는 경우 e
+		
+		else if(select2 == 2) { System.out.println("알림)) 2층은 현재 층입니다."); }
+		// 2층은 현재층
+		else if(select2 == 3) { // 2층에서 3층으로 가는 경우
+			System.out.println("3층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("3층에 도착했습니다.");
+		}
+		
+		else if(select2 == 4) { // 2층에서 4층으로 가는 경우
+			System.out.println("4층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("4층에 도착했습니다.");
+		}
+		
+		else if(select2 == 5) { // 2층에서 5층으로 가는 경우
+			System.out.println("5층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("5층에 도착했습니다.");
+		}
+	} // 2층 e
+	
+	else if(select == 3) { // 3층
+		System.out.printf("현재 %d층에 있습니다.\n", select);
+		System.out.println("몇층으로 가기를 원하십니까");
+		int select2 = scanner.nextInt();
+		if (select2 ==1 ) { // 3층에서 1층으로 가는 경우
+			System.out.println("1층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("1층에 도착했습니다.");
+		}
+		else if(select2 == 2) { // 3층에서 2층으로 가는 경우
+			System.out.println("2층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("2층에 도착했습니다.");
+		}
+		else if(select2 == 3) {System.out.println("알림)) 3층은 현재 층입니다.");}
+		else if(select2 == 4) { // 3층에서 4층으로 가는 경우
+			System.out.println("4층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("4층에 도착했습니다.");
+		}
+		else if(select2 == 5) { // 3층에서 5층으로 가는 경우
+			System.out.println("5층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("5층에 도착했습니다.");
+		}
+	}
+	else if(select == 4) {
+		System.out.printf("현재 %d층에 있습니다.\n", select);
+		System.out.println("몇층으로 가기를 원하십니까");
+		int select2 = scanner.nextInt();
+		if(select2 == 1) {
+			System.out.println("1층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("1층에 도착했습니다.");
+		}
+		else if(select2 == 2) {
+			System.out.println("2층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("2층에 도착했습니다.");
+		}
+		else if(select2 == 3) {
+			System.out.println("3층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("3층에 도착했습니다.");
+		}
+		else if(select2 == 4) {System.out.println("알림)) 4층은 현재 층입니다.");}	
+		else if(select2 == 5) {
+			System.out.println("5층으로 이동합니다");
+			for(int i = 1; i<select2; i++) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("5층에 도착했습니다.");
+		}
+		
+	}
+	else if(select == 5) {
+		System.out.printf("현재 %d층에 있습니다.\n", select);
+		System.out.println("몇층으로 가기를 원하십니까");
+		int select2 = scanner.nextInt();
+		if(select2 == 1) {
+			System.out.println("1층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("1층에 도착했습니다.");
+		}
+		else if(select2 == 2) {
+			System.out.println("2층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("2층에 도착했습니다.");
+		}
+		else if(select == 3) {
+			System.out.println("3층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("3층에 도착했습니다.");
+		}
+		else if(select == 4) {
+			System.out.println("4층으로 이동합니다");
+			for(int i = 1; i<select2; i--) { // f s
+				 System.out.printf("%d\n", i);
+			} // f e
+			System.out.println("4층에 도착했습니다.");
+		}
+		else if(select2 ==5) {System.out.println("알림)) 5층은 현재 층입니다.");}
+	}
+	else System.out.println("잘못된 층을 입력하셨습니다. [재입력]");	
+		} // w e
+	} // m e
+} // c e
