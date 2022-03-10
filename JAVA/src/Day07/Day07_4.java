81b3ac67f4b173f745c468f999fffb5b2c754bf5
504 <504@DESKTOP-0MVNHLQ>
2022-03-10 12:04:57 +0900

    d

--- JAVA/src/Day07/Day07_4.java
+++ JAVA/src/Day07/Day07_4.java
@@ -6,11 +6,12 @@ public static void main(String[] args) {
 	// 1. ***** 객체 선언 ******
 	Car2 mycar = new Car2();
 	
-	mycar.keyTurnon(); // 2. 자동차 시동 걸기 인수x,반환x
+	mycar.keyTurnon(); // 2. 자동차 시동 걸기 (인수 x, 반환 x)
 	
-	mycar.run(); // 3. 자동차 전진 인수x,반환x
+	mycar.run(); // 3. 자동차 전진 (인수 x, 반환 x)
 	
-	int speed = mycar.getspeed(); // 4. 차의 현재 속도 확인
+	int speed = mycar.getspeed(); 
+	// 4. 차의 현재 속도 확인 (인수 x, 반환 o)
 	System.out.println("현재 속도 : " + speed + "km/h");
 	
 	
