7c001ca3e5250514f5720eb6719c4665a25a21e0
JSB95 <vetements@kakao.com>
2022-03-17 20:06:21 +0900

    day12_도서대여과제

--- JAVA/src/Day12/Day12_2.java
+++ JAVA/src/Day12/Day12_2.java
@@ -15,7 +15,10 @@ public class Day12_2
 		case 2 :
 		case 4 : System.out.println("여자"); break;
 		}
-		
+		// 자바 메모리[JVM]
+			// 스택 메모리 : 지역변수
+			// 힙 메모리 :  객체
+			// 메소드 메모리 : 
 		// p. 501 equals() : 문자열 비교
 			// 기본자료형 사용하는 변수는 연산자 사용가능 [ == ]
 			// 문자형 사용하는 변수는 연산자 사용불가능 [ equals ]
@@ -28,6 +31,79 @@ public class Day12_2
 		} else System.out.println("다른 String 객체를 참조");
 		if(strVar1.equals(strVar2)) System.out.println("같은 문자열을 가짐");
 		else System.out.println("다른 문자열을 가짐");
+		
+		// getBytes() : 문자열 -> 바이트열 변환
+		String str = "안녕하세요";
+		
+		byte[] bytes1 = str.getBytes(); // 문자열 -> 바이트열 변환
+		System.out.println("영문은 1바이트 한글2바이트 : " + bytes1.length); // 배열명.length : 배열길이 필드
+		String str1 = new String(bytes1); // 바이트열 -> 문자열 변환
+		System.out.println("바이트열 -> 문자열 : " + str1);
+		try { // 인코딩타입[인코딩/디코딩 : 변환방식]
+		// 1. EUC-KR [한글/영문 : 한글 2바이트]
+		byte[] bytes2 = str.getBytes("EUC-KR"); // 일반 예외 발생, 아스키코드에 한글 추가
+		System.out.println("EUC-KR 길이 : " + bytes2.length);
+		String str2 = new String(bytes2, "EUC-KR");
+		System.out.println("바이트열 -> 문자열 : " + str2);
+		
+		// 2. UTF-8 : 전세계 공용어 사용 가능(한글 3바이트)
+		byte[] bytes3 = str.getBytes("UTF-8"); // 유니코드
+		System.out.println("UTF-8 길이 : " + bytes3.length);
+		String str3 = new String(bytes3, "UTF-8");
+		System.out.println("바이트열 -> 문자열 : " + str3);
+		
+		}catch(Exception e) {}
+		
+		// indexof : 문자열내 특정 문자열 위치[인덱스] 찾기
+		String subject = "자바 프로그래밍";
+		int location = subject.indexOf("프로그래밍"); 
+		System.out.println(location);
+		
+		if(subject.indexOf("자바") != -1) { // 인덱스가 0번에서 시작 : -1 인덱스가 없다
+			System.out.println("자바와 관련 책");
+		} else System.out.println("자바와 관련없는 책");
+		
+		String ssn2 = "7306241230123";
+		int length = ssn2.length();// 문자열내 문자 개수(=길이 구하기)
+		if(length == 13) { // 유효성검사 시 사용
+			System.out.println("주민번호 자릿수가 맞습니다.");
+		} else System.out.println("주민번호 자릿수가 틀립니다.");
+		
+		System.out.println("------------------------------");
+		// p. 506 replace("기존문자", "새로운문자"); : 문자열 대치
+		String oldStr = "자바는 개체지향언어 입니다. 자바는 풍부한 API를 지원합니다.";
+		String newStr = oldStr.replace("자바","JAVA");
+		System.out.println("기존문자열 : " + oldStr);
+		System.out.println("새로운문자열 : " + newStr);
+		
+		// substring() vs split(): 문자열 자르기
+		String ssn3 = "880815-1234567";
+		String firstNum = ssn3.substring(0,6); // 시작인덱스(0) - 마지막인덱스(6)
+		System.out.println("확인 : " + firstNum);
+		
+		String secondNum = ssn3.substring(7); // 마지막인덱스부터 (7)
+		System.out.println("확인 : " + secondNum);
+		System.out.println("------------------------------");
+		// substring() : 인덱스 기준, split() : 문자 기준
+		// "880815-1234567"를 -을 기준으로 split으로 자를 시 2부분으로 나뉨 -> 배열로 반환 
+		// -을 기준으로 split으로 자를 시 2부분으로 나뉨 -> "-"출력 X
+		System.out.println("앞부분 확인 : " + ssn3.split("-")[0]);
+		System.out.println("뒷부분 확인 : " + ssn3.split("-")[1]);
+		// 영문문자열.toLowerCase -> 영문을 소문자 
+		// 영문문자열.toUpperCase -> 영문을 대문자
+		// 문자열.trim() : 앞과 뒤의 공백 제거[중간에 띄어쓰기는 제거하지 않음] -> 외부로부터 문자 자료 삽입시 예상치 못한 공백 있을 시 사용
+		
+		// valueOf() : 기본타입[int, double 등] -> 문자열 변환
+		String str2 = String.valueOf(10); // int형 10을 -> String 10으로 변환
+		String str3 = String.valueOf(10.5); // double형 10.5를 -> 문자열 10.5로 변환
+		String str4 = String.valueOf(true); // boolean형 true -> 문자열 true
+		String str5 = 10+""; // int형을 String변수에 저장[불가능] -> +연산자를 통해 공백 삽입하면 문자열로 저장 가능
+
+		System.out.println(str2 + 10);  // 문자 + 문자 : 연결 -> 1010 출력
+		System.out.println(str3 + 10);
+		// if(str4) 오류 : str5에는 boolean 형이 아닌 문자형 true 저장
+ 	
+	
 	}
 
 }
--- JAVA/src/과제/도서대여프로그램.java
+++ JAVA/src/과제/도서대여프로그램.java
@@ -1,28 +1,169 @@
 package 과제;
 
+import java.util.Scanner;
+
 public class 도서대여프로그램
 {
 	// 도서 대여 프로그램
-			// 1. 배열 변수
-			// 1-1. 회원[아이디(중복 X), 비밀번호]
-			// 1-2. 도서[도서명(중복 X), 도서대여여부]
-			// 2. 초기메뉴
-			// 2-1. 회원가입 // 2-2. 로그인
-			// 2-3. 회원가입시 중복체크
-			// 3. 로그인시 메뉴
-			// 3-1. 도서검색 // 3-2. 도서목록 // 3. 도서대여 // 4. 도서반납 // 5. 로그아웃
-			// + 도서검색시 도서명이 일치하면 도서명과 도서대여여부 출력
-			// + 도서 목록시 모든 도서명 출력
-			// + 도서대여시 도서대여여부가 가능할때 도서대여 처리
-			// + 도서반납시 본인이 대여한 도서만 반납 처리
-			// + 로그아웃
-			// 로그인시 아이디가 admin이면 관리자메뉴
-			// 1. 도서 등록 2. 도서 메뉴 3. 도서삭제(도전) 4. 로그아웃
-			// + 도서 등록시 도서명 입력받아 도서 등록처리
-			// + 도서 목록시 모든 도서명 출력 // 도서삭제시 삭제할 도서명 입력받아 동일한 도서명 삭제(NULL)
-			// 로그아웃시 초기메뉴로
+	// 1. 배열 변수
+	// 1-1. 회원[아이디(중복 X), 비밀번호]
+	// 1-2. 도서[도서명(중복 X), 도서대여여부]
+	// 2. 초기메뉴
+	// 2-1. 회원가입 // 2-2. 로그인
+	// 2-3. 회원가입시 중복체크
+	// 3. 로그인시 메뉴
+	// 3-1. 도서검색 // 3-2. 도서목록 // 3. 도서대여 // 4. 도서반납 // 5. 로그아웃
+	// + 도서검색시 도서명이 일치하면 도서명과 도서대여여부 출력
+	// + 도서 목록시 모든 도서명 출력
+	// + 도서대여시 도서대여여부가 가능할때 도서대여 처리
+	// + 도서반납시 본인이 대여한 도서만 반납 처리
+	// + 로그아웃
+	// 로그인시 아이디가 admin이면 관리자메뉴
+	// 1. 도서 등록 2. 도서 메뉴 3. 도서삭제(도전) 4. 로그아웃
+	// + 도서 등록시 도서명 입력받아 도서 등록처리
+	// + 도서 목록시 모든 도서명 출력 // 도서삭제시 삭제할 도서명 입력받아 동일한 도서명 삭제(NULL)
+	// 로그아웃시 초기메뉴로
 	public static void main(String[] args)
 	{
-		
+		String[][] memberlist = new String[100][2];
+		String[][] booklist = new String[100][2];
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("도서 대여 프로그램");
+			System.out.println("1. 회원 가입 2. 로그인 3. 로그아웃");
+			int select = sc.nextInt();
+			if(select == 1) {
+				System.out.println("회원 가입 페이지");
+				System.out.println("아이디 : ");     String id = sc.next();
+				System.out.println("비밀번호 : ");   String pw = sc.next();
+				boolean idcheck = false;
+				for(int i = 0; i < memberlist.length; i++) {
+					if(memberlist[i][0] == null && memberlist[i][1] == null) {
+						System.out.println("회원가입이 완료되었습니다.");
+						memberlist[i][0] = id;
+						memberlist[i][1] = pw;
+						idcheck = true;
+						break; }
+				for(int j = 0; j < memberlist.length; j++) {
+					if(memberlist[j][0] != null && memberlist[j][0].equals(id)) {
+						System.err.println("아이디가 중복됩니다. 다시 입력해주세요.");
+						idcheck = false;
+						break;
+						}
+					}
+				}
+			}
+			else if(select == 2) { // else if 2 s
+				System.out.println("로그인 페이지입니다.");
+				System.out.println("아이디를 입력해주세요");			String id = sc.next();
+				System.out.println("비밀번호를 입력해주세요");	 	String pw = sc.next();
+				boolean logincheck = false;
+				while(true) {// w2 s
+				for(int i = 0; i < memberlist.length; i++) { // for1 s
+					if(memberlist[i][0] != null && memberlist[i][0].equals(id) && memberlist[i][1].equals(pw) ) { // if 1 s
+						System.out.println("로그인이 성공했습니다.");
+						logincheck = true;
+					
+					if(logincheck && memberlist[i][0].equals("admin") && memberlist[i][1].equals(pw)) {
+						
+						System.out.println("관리자 모드로 로그인합니다.");
+						System.out.println("1. 도서 등록 2. 도서 메뉴 3. 도서삭제(도전) 4. 로그아웃");
+						int select1 = sc.nextInt();
+						boolean titlecheck = false;
+						if (select1 == 1) {
+							System.out.println("도서를 등록합니다.");
+							System.out.println("도서의 제목을 입력해주세요");  String title = sc.next();
+							for(int j = 0; j<booklist.length; j++) {
+								if(booklist[j][0] == null) {
+								System.out.println("도서등록이 완료되었습니다."); 
+								booklist[j][0] = title;		booklist[j][1] = "대여 가능"; titlecheck = true; break;}
+								for(int x = 0; x<booklist.length; x++) {
+									if(booklist[j][0] != null && booklist[j][0].equals(title)) {
+										System.out.println("도서의 제목이 중복됩니다. 다시 입력해주세요");
+										titlecheck = false; break;
+									}	
+								}
+							}
+						}
+						else if (select1 == 2) {
+							for(int j = 0; j < booklist.length; j++) {
+								if(booklist[j][0] != null) 
+									{System.out.println("도서목록을 출력합니다."); System.out.print(booklist[j][0] + "\n"); break;}
+							}
+						}
+						else if(select1 == 3) { // 도서 삭제
+							System.out.println("도서를 삭제합니다.");
+							System.out.println("삭제할 도서의 제목을 입력해주세요"); String title = sc.next();
+							for(int j = 0; j<booklist.length; j++) {
+								if(booklist[j][0] != null && booklist[j][0].equals(title) && booklist[j][1].equals("대여 가능")) 
+								{
+									System.out.println("도서를 삭제합니다.");
+									booklist[j][0] = null; 		booklist[j][1] = null; 
+									booklist[j-1][0] = null;	booklist[j-1][1] = null;		break;
+								}
+								else System.out.println("삭제하실 도서가 없습니다"); break;
+								
+							}
+						}	
+						else if(select1 == 4) {System.out.println("로그아웃!!"); break;}
+					}	 
+					else if (logincheck) {
+						
+						System.out.println("회원 로그인");
+						System.out.println("1. 도서검색 // 2. 도서목록 // 3. 도서대여 // 4. 도서반납 // 5. 로그아웃");
+						int select1 = sc.nextInt();
+						if(select1 == 1) {
+							System.out.println("도서를 검색합니다.");
+							System.out.println("검색할 도서의 제목을 입력해주세요"); String title = sc.next();
+							for(int j = 0; j<booklist.length; j++) {
+								if(booklist[j][0] != null && booklist[j][0].equals(title)) {
+									System.out.println("검색한 도서의 제목을 찾았습니다.");
+									System.out.println(title); break;
+								}
+								else System.out.println("검색하신 도서가 없습니다"); break;
+							}
+						}
+						else if(select1 == 2) {
+							for(int j = 0; j < booklist.length; j++) {
+								if(booklist[j][0] != null) System.out.println("도서목록을 출력합니다."); System.out.print(booklist[j][0] + "\t");
+							}
+						}
+						else if(select1 == 3) {
+							System.out.println("도서를 대여합니다.");
+							System.out.println("대여하실 도서의 이름을 입력하세요."); String title = sc.next();
+							for(int j = 0; j<booklist.length; j++) {
+								if(booklist[j][0] != null && booklist[j][0].equals(title) && booklist[j][1].equals("대여 가능")) {
+									System.out.println("도서 대여가 완료되었습니다.");
+									booklist[j][1] = "대여 중"; break;
+								}
+							}
+						}
+						else if(select1 == 4) {
+							System.out.println("도서를 반납합니다.");
+							System.out.println("반납하실 도서의 이름을 입력하세요."); String title = sc.next();
+							for(int j = 0; j<booklist.length; j++) {
+								if(booklist[j][0] != null && booklist[j][0].equals(title) && booklist[j][1].equals("대여 중")) {
+									System.out.println("도서 반납이 완료되었습니다."); booklist[j][1] = "대여 가능"; break;
+								}
+							}
+						}
+						else if(select1 == 5) {System.out.println("로그아웃!"); break;}
+						else {System.out.println("잘못입력하셨습니다. 다시 입력해주세요.");}
+								} 
+									
+					else {System.out.println("로그인 실패 다시 입력해주세요");}
+						} // if1 e
+					} // for1 e
+				} // w2 e
+			} // else if 2 e
+			
+			
+			else if(select == 3) {
+				System.out.println("로그아웃 되셨습니다."); break;
+			}
+			else {System.out.println("잘못입력하셨습니다.");}
+			
+		} // w1 e
 	} // me
-} // ce
+}
+// ce
