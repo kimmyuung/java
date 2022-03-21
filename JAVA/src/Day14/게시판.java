4df3d7cda339e14bbc62987dabe0f187194194a6
504 <504@DESKTOP-HH4VOLC>
2022-03-21 12:12:48 +0900

    day14

---
+++ JAVA/src/Day14/게시판.java
@@ -0,0 +1,19 @@
+package Day14;
+
+import java.util.ArrayList;
+
+public class 게시판 {
+
+	// 1. field
+	private String title; 		
+	private String context;
+	private String writer;
+	private int point;
+	private 게시판 게시판 = new 게시판();
+	private ArrayList<String> list = new ArrayList<>();
+	private 게시판[] 게시판목록 = new 게시판[100];
+	
+	// 2. constructor
+	
+	// 3. method
+}
---
+++ JAVA/src/Day14/컨트롤러.java
@@ -0,0 +1,24 @@
+package Day14;
+
+public class 컨트롤러 {
+public boolean 입차(String 차량번호) {
+	
+	return true;
+}
+
+public static boolean 입차2(String 차량번호) {
+	
+	return true;
+}
+
+public void 출차(String 차량번호) {
+	
+	// void는 return 생략 가능
+	
+}
+
+public String 메출() {
+	
+	return "오늘 매출 : 00000원";
+}
+}
