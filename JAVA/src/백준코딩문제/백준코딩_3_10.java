dd0891d462228cd692b462b8080f3ead5466bd5c
504 <504@DESKTOP-HH4VOLC>
2022-04-12 21:40:55 +0900

    ㅇ

--- JAVA/src/백준코딩문제/백준코딩_3_10.java
+++ JAVA/src/백준코딩문제/백준코딩_3_10.java
@@ -1,20 +1,18 @@
 package 백준코딩문제;
 
-
 import java.util.Scanner;
 
-public class 백준코딩_3_9 {
+public class 백준코딩_3_10 { // 3-9의 반대
 public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	
 	int star = sc.nextInt();
-	
+	sc.close();
 	for(int i = 0; i < star; i++) {
-		for(int j = 0; j<i+1; j++) {
+		for(int j = 0; j < star-i; j++) {
 			System.out.print("*");
 		}
 		System.out.println();
 	}
-	
 }
 }
